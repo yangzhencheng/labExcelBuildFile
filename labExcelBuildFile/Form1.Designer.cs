752d29f044213c12667a06338b8e3da82938ccde
yangzhencheng <724207239@qq.com>
2016-11-16 15:55:24 +0800

    Add files via upload

---
+++ labExcelBuildFile/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace labExcelBuildFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.labFilePath = new System.Windows.Forms.Label();
+            this.btnBuildSave = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSQL = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "打开Excel";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // labFilePath
+            // 
+            this.labFilePath.AutoSize = true;
+            this.labFilePath.Location = new System.Drawing.Point(15, 48);
+            this.labFilePath.Name = "labFilePath";
+            this.labFilePath.Size = new System.Drawing.Size(77, 12);
+            this.labFilePath.TabIndex = 1;
+            this.labFilePath.Text = "已打开文件：";
+            // 
+            // btnBuildSave
+            // 
+            this.btnBuildSave.Enabled = false;
+            this.btnBuildSave.Location = new System.Drawing.Point(117, 12);
+            this.btnBuildSave.Name = "btnBuildSave";
+            this.btnBuildSave.Size = new System.Drawing.Size(75, 23);
+            this.btnBuildSave.TabIndex = 2;
+            this.btnBuildSave.Text = "建立并保存";
+            this.btnBuildSave.UseVisualStyleBackColor = true;
+            this.btnBuildSave.Click += new System.EventHandler(this.btnBuildSave_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(17, 75);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 23;
+            this.dataGridView.Size = new System.Drawing.Size(755, 335);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(218, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "目标SQL：";
+            // 
+            // txtSQL
+            // 
+            this.txtSQL.Location = new System.Drawing.Point(284, 13);
+            this.txtSQL.Name = "txtSQL";
+            this.txtSQL.Size = new System.Drawing.Size(488, 21);
+            this.txtSQL.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 422);
+            this.Controls.Add(this.txtSQL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnBuildSave);
+            this.Controls.Add(this.labFilePath);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Excel生成文件工具";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label labFilePath;
+        private System.Windows.Forms.Button btnBuildSave;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSQL;
+    }
+}
+
---
+++ labExcelBuildFile/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labExcelBuildFile
+{
+    public partial class Form1 : Form
+    {
+        private string _filePath = "";
+        private DataTable dt = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "Excel 2003 (*.xls)|*.xls";
+            fd.ShowReadOnly = true;
+            DialogResult r = fd.ShowDialog();
+            
+            if(r != DialogResult.OK) return;
+
+            _filePath = fd.FileName;
+            labFilePath.Text = "打开文件：" + _filePath;
+            btnBuildSave.Enabled = true;
+
+            string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + _filePath + ";Extended Properties=Excel 8.0;";
+            using (OleDbConnection conn = new OleDbConnection(strConn))
+            {
+                conn.Open();
+
+                using (OleDbDataAdapter ad = new OleDbDataAdapter("select * from [Sheet1$]", conn))
+                {
+                    dt = new DataTable();
+                    ad.Fill(dt);
+                }
+            }
+
+            dataGridView.DataSource = dt;
+        }
+
+
+        private void btnBuildSave_Click(object sender, EventArgs e)
+        {
+            if(0 == txtSQL.Text.Trim().Length)
+            {
+                MessageBox.Show("请先输入语句！");
+                return;
+            }
+
+            // string strSQL = "insert into [wj_peixun].[dbo].[User_List]([Cn_Name], [En_Name], [Sex], [Birthday], [Photo], [Nationality_Code], [CardID], [Company], [ProvinceID], [CityID], [ClassCode], [UserID], [UserCode], [Current_Dan], [Dan_Num], [Current_Dan_Poom_Type], [Current_Dan_PassDate], [Request_Dan], [Request_Dan_Poom_Type], [Phone], [Address], [PeiXun_Id], [Intro], [nianxian], [IsUser], [Cta_UserCheck_Flags], [IsSuccess], [CreateTime]) values('__Cn_Name__', '__En_Name__', __Sex__, '__Birthday__', '__Photo__', '__Nationality_Code__', '__CardID__', '__Company__', __ProvinceID__, __CityID__, '__ClassCode__', '__UserID__', '__UserCode__', __Current_Dan__, '__Dan_Num__', __Current_Dan_Poom_Type__, '__Current_Dan_PassDate__', __Request_Dan__, __Request_Dan_Poom_Type__, '__Phone__', '__Address__', __PeiXun_Id__, '__Intro__', __nianxian__, __IsUser__, __Cta_UserCheck_Flags__, __IsSuccess__, '__CreateTime__');";
+            string strSQL = txtSQL.Text.Trim();
+            string midstr = "";
+            string midSQL = "";
+
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = "Text File (*.txt)|*.txt";
+            DialogResult r = sf.ShowDialog();
+
+            if(r != DialogResult.OK)
+            {
+                return;
+            }
+
+            string _saveFile = sf.FileName;
+
+
+            
+
+            int totCoumnCount = dt.Columns.Count;
+
+            for (int i = 0; i < totCoumnCount; i++)
+            {
+                if (midstr.Length > 0) midstr += "|";
+                midstr += dt.Columns[i].ColumnName;
+            }
+
+            string[] labTab = midstr.Split('|');
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                midSQL = strSQL;
+
+
+                for (int j = 0; j < totCoumnCount; j++)
+                {
+
+                    midSQL = midSQL.Replace("__" + labTab[j] + "__", dt.Rows[i][labTab[j]].ToString());
+                }
+
+                sb.Append(midSQL + "\r\n");
+            }
+
+            if (File.Exists(_saveFile))
+            {
+                File.Delete(_saveFile);
+            }
+
+            FileStream fs = new FileStream(_saveFile, FileMode.CreateNew);
+            StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+            sw.Write(sb.ToString());
+            sw.Close();
+            fs.Close();
+
+            MessageBox.Show("操作完成！");
+        }
+    }
+}
